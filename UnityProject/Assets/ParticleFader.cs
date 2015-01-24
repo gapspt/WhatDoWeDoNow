c9f8d7a3d212eafaf9bcf047d6c811b86697dee5
Bruno Santos <bmfs@eksperimental.net>
2015-01-24 22:14:31 +0000

    Added new attacks

---
+++ UnityProject/Assets/ParticleFader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleFader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("clearParticle");
+	}
+	
+	private IEnumerator clearParticle() {
+		yield return new WaitForSeconds(3);
+		Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
