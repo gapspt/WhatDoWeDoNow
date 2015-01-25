33c3a3e7a4c38cbc4a2eb3d37f2b01aeb23c8314
gaps <gaps@noemail.pt>
2015-01-25 15:08:05 +0000

    NPC triggers

---
+++ UnityProject/Assets/Scripts/NPCTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCTrigger : MonoBehaviour {
+    // Actions to do
+    public bool Jump = true;
+    public bool Reverse = false;
+
+    // From where
+    public bool FromLeft = true;
+    public bool FromRight = true;
+
+    void Awake()
+    {
+        if (!Debug.isDebugBuild)
+        {
+            foreach (var c in GetComponents<Renderer>())
+            {
+                Destroy(c);
+            }
+        }
+    }
+}
