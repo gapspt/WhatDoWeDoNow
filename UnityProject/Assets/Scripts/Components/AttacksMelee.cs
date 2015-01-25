4b1ab0def26c390e09fb72fb9b5ce420b94fda46
gaps <gaps@noemail.pt>
2015-01-25 14:00:59 +0000

    Some improvements on the map and bugfixes

--- UnityProject/Assets/Scripts/Components/AttacksMelee.cs
+++ UnityProject/Assets/Scripts/Components/AttacksMelee.cs
@@ -55,11 +55,13 @@ public class AttacksMelee : Attack {
 	
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		Debug.Log ("colliding with " + col.gameObject.name + " tag:" + col.gameObject.tag + " attack:" + attackTag);
-		
+   	    //Debug.Log ("colliding with " + col.gameObject.name + " tag:" + col.gameObject.tag + " attack:" + attackTag);
 		if (col.gameObject.tag == attackTag){
-			col.gameObject.GetComponent<Player>().attack(attackDamage);
+            Player player = col.gameObject.GetComponent<Player>();
+            if (player)
+            {
+                player.attack(attackDamage);
+            }
 		}
-		//Destroy(col.gameObject);
 	}
 }
